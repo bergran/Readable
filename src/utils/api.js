bf8c3e29dd7f9748a74d519e44e7f1abf2503ada
Ángel Berhó <bergraan@gmail.com>
2017-09-23 23:30:58 +0200

    feat: Added more post object request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,7 @@
 let authorization = localStorage.Authorization
 const host = process.env.REACT_APP_HOST
+const upVote = 'upVote'
+const downVote = 'downVote'
 if (!authorization) {
   authorization = localStorage.Authorization = Math.ceil(Math.random() * 100)
 }
@@ -29,3 +31,57 @@ export const getCommentsPost = post => {
     return fetch(`http://${host}:3001/posts/${post}/comments`, {headers})
         .then(data => data.json())
 }
+
+export const getComment = comment => {
+  return fetch(`http://${host}:3001/comments/${comment}`, {headers})
+        .then(data => data.json())
+}
+
+export const getPost = post => {
+  return fetch(`http://${host}:3001/posts/${post}`, {headers})
+        .then(data => data.json())
+}
+
+export const addPost = ({
+    id,
+    title,
+    timestamp,
+    body,
+    author = authorization,
+    category
+  }) => {
+  return fetch(`http://${host}:3001/posts`, {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      id,
+      title,
+      timestamp,
+      body,
+      author,
+      category
+    })
+  })
+    .then(data => data.json())
+}
+
+const votePost = (post, option) => {
+  return fetch(`http://${host}:3001/posts/${post}`, {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      option
+    })
+  })
+    .then(data => data.json())
+}
+
+export const voteUpPost = post => votePost(post, upVote)
+
+export const voteDownPost = post => votePost(post, downVote)
