d79dc420092f0076f3b49e7e2b99b089a3104807
Ángel Berhó <bergraan@gmail.com>
2017-09-26 21:14:04 +0200

    feat: Added "Add comment" action on reducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,9 @@
+import { ADD_COMMENT } from '../actions/comment'
+
 export const comments = (state = [], action) => {
   switch (action.type) {
+    case ADD_COMMENT :
+        return state.filter(comment => comment.id !== action.comment.id).concat(action.comment)
     default:
       return state
   }
