dcfc3d82ab98ff4e343736525c657214cc1a7567
bergran <bergraan@gmail.com>
2017-10-24 17:10:47 +0200

    feat: create state popup on redux and his actions

--- src/reducers/combineReducers.js
+++ src/reducers/combineReducers.js
@@ -2,14 +2,13 @@ import { categories } from './categories'
 import { comments } from './comments'
 import { posts } from './posts'
 import { user } from './user'
-import { updateTime} from "./updateTime";
+import { popup } from "./popup";
 import { combineReducers } from 'redux'
-
 
 export const reducers = combineReducers({
     categories,
     comments,
     posts,
     user,
-    updateTime
+    popup
 })
