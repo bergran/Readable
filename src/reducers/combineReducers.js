807a0d818a33803d067f806ee35d938db0ac832c
bergran <bergraan@gmail.com>
2017-10-26 09:27:06 +0200

    fix: removed user from reducers

--- src/reducers/combineReducers.js
+++ src/reducers/combineReducers.js
@@ -1,7 +1,6 @@
 import { categories } from './categories'
 import { comments } from './comments'
 import { posts } from './posts'
-import { user } from './user'
 import { popup } from "./popup";
 import { combineReducers } from 'redux'
 
@@ -9,6 +8,5 @@ export const reducers = combineReducers({
     categories,
     comments,
     posts,
-    user,
     popup
 })
